2855c05103d2b2e1fcf98554222ab201bd588cbe
Thom Le <thomle0418@gmail.com>
2022-06-05 00:51:21 -0600

    created hook for add to cart button

--- Documents/projects/ecommerce-tutorial/ecommerce/context/StateContext.js
+++ Documents/projects/ecommerce-tutorial/ecommerce/context/StateContext.js
@@ -5,10 +5,34 @@ const Context = createContext();
 
 export const StateContext =({children})=>{
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
+    const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
     const [totalQuantities, setTotalQuantities] = useState();
     const [qty, setQty] = useState(1);
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item)=> item._id === product._id);
+
+        setTotalPrice((prevTotalPrice)=>prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities)=>prevTotalQuantities + product.quantity);
+
+        if(checkProductInCart){
+            const updatedCartItems = cartItems.map((cartProduct)=>{
+                if(cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+
+            setCartItems(updatedCartItems);
+        }else {
+            product.quantity =quantity;
+
+            setCartItems([...cartItems, {...product}]);
+        }
+
+        toast.success(`${qty} ${product.name} added to the cart.`);
+    }
 
     //increase quantity
     const incQty = () =>{
@@ -34,6 +58,7 @@ export const StateContext =({children})=>{
             qty, 
             incQty,
             decQty,
+            onAdd,
         }}
         >
             {children}
--- Documents/projects/ecommerce-tutorial/ecommerce/pages/product/[slug].js
+++ Documents/projects/ecommerce-tutorial/ecommerce/pages/product/[slug].js
@@ -7,7 +7,7 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products}) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty }=useStateContext();
+    const { decQty, incQty, qty, onAdd }=useStateContext();
   return (
     <div>
         <div className='product-detail-container'>
@@ -61,7 +61,7 @@ const ProductDetails = ({ product, products}) => {
                     <div className='buttons'>
                         <button type='button' 
                         className='add-to-cart' 
-                        onClick=''>
+                        onClick={()=> onAdd(product, qty)}>
                             Add to Cart
                         </button>
 
